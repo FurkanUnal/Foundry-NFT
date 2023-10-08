3c8bc75f2e6362cbdb4c2b85670e222331d21c9b
FurkanUnal <unal_furkan@outlook.com>
2023-10-08 11:20:49 +0300

    Deployed BasicNft to Sepolia Testnet

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
-import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script {
